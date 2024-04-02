089b3a5efa0b847da20d8eb6704ae1b51bdcc571
heyejiyang <xoxo99312@naver.com>
2024-04-02 16:14:31 +0900

    react 컴포넌트

---
+++ 2. 실무프로젝트(p-1)/exam01/src/App.js
@@ -0,0 +1,7 @@
+import MyComponent from './components/MyComponent';
+
+const App = () => {// 문자열을 제외한 모든 값은 중괄호 안에 설정한다.
+  return <MyComponent  name = {999} num={11}/>;
+};
+
+export default App;
---
+++ 2. 실무프로젝트(p-1)/exam01/src/components/MyComponent.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';//자료형 한정 (경고를뜨게해줌)
+
+const MyComponent = ({name, num}) => {//더 짧게 매개변수로 분해해서 쓰자
+    return( 
+    <>
+        <div>안녕하세요!, 제 이름은 {name}입니다.</div>
+        <div>제가 좋아하는 숫자는 {num}입니다. </div>
+    </>
+    );
+};
+
+MyComponent.defaultProps = { //값이 없을때 기본값 설정
+    name:'기본이름이다 냥',
+};
+
+MyComponent.propTypes = { //문자열이 아니면 경고뜨게한다.
+    name: PropTypes.string,
+}
+
+export default MyComponent;
