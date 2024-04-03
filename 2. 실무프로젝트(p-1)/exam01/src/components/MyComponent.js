f3c3647ea03577c240093a69995a0b2b030c2ba7
heyejiyang <xoxo99312@naver.com>
2024-04-03 12:10:28 +0900

    함수형컴포넌트

--- 2. 실무프로젝트(p-1)/exam01/src/components/MyComponent.js
+++ 2. 실무프로젝트(p-1)/exam01/src/components/MyComponent.js
@@ -1,25 +1,26 @@
-import PropTypes from 'prop-types';//자료형 한정 (경고를뜨게해줌)
-
+import PropTypes from 'prop-types'; //자료형 한정 (경고를뜨게해줌)
 
 //함수형 컴포넌트
-const MyComponent = ({name, num,children}) => {//더 짧게 매개변수로 분해해서 쓰자
-    return( 
+const MyComponent = ({ name, num, children }) => {
+  //더 짧게 매개변수로 분해해서 쓰자
+  return (
     <>
-        <div>안녕하세요!, 제 이름은 {name}입니다.</div>
-        <div>제가 좋아하는 숫자는 {num}입니다. </div>
-        {children}
+      <div>안녕하세요!, 제 이름은 {name}입니다.</div>
+      <div>제가 좋아하는 숫자는 {num}입니다. </div>
+      {children}
     </>
-    );
+  );
 };
 
-MyComponent.defaultProps = { //값이 없을때 기본값 설정
-    name:'기본이름이다 냥',
-  
-}; 
+MyComponent.defaultProps = {
+  //값이 없을때 기본값 설정
+  name: '기본이름이다 냥',
+};
 
-MyComponent.propTypes = { //문자열이 아니면 경고뜨게한다.
-    name: PropTypes.string,
-    num: PropTypes.number.isRequired,
-}
+MyComponent.propTypes = {
+  //문자열이 아니면 경고뜨게한다.
+  name: PropTypes.string,
+  num: PropTypes.number.isRequired,
+};
 
 export default MyComponent;
