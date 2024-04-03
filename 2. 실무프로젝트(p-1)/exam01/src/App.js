0206c137e013d70f590d81e479b42899517a0d95
heyejiyang <xoxo99312@naver.com>
2024-04-03 12:12:06 +0900

    컴포넌트 부모

--- 2. 실무프로젝트(p-1)/exam01/src/App.js
+++ 2. 실무프로젝트(p-1)/exam01/src/App.js
@@ -1,7 +1,20 @@
-import MyComponent from './components/MyComponent';
+//import MyComponent from './components/MyComponent';
+//import MyComponent from './components/MyComponent2';
+//import Counter from "./components/Counter2";
+import Profile from "./components/Profile";
 
 const App = () => {// 문자열을 제외한 모든 값은 중괄호 안에 설정한다.
-  return <MyComponent  name = {999} num={11}/>;
+  //return <Counter/>;
+  return <Profile/>;
+
+
+  /*
+  return (
+  <MyComponent  name = "클래스이름" num={3}>
+    <h1>반갑습니다.</h1>
+  </MyComponent>
+//태그가 복수개면 소괄호로 감싸야함
+  );*/
 };
 
 export default App;
