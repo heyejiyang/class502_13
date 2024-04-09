57573dd6f34cc630ddf881ee39d109895cd768b2
heyejiyang <xoxo99312@naver.com>
2024-04-09 09:30:44 +0900

    classname까지

--- 2. 실무프로젝트(p-1)/exam05/src/components/CSSModule.js
+++ 2. 실무프로젝트(p-1)/exam05/src/components/CSSModule.js
@@ -1,8 +1,10 @@
 import styles from './CSSModule.module.scss';
 import { useState } from 'react';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 
 console.log(styles);
+
+const cx = classNames.bind(styles); //this의 범위를 바꾼다. styles로 고정 새로운 함수를 만들어줌(호출은 하지않는다.)
 
 const CSSModule = () => {
   const [visible, setVisible] = useState(false); //visible값 false로 초기설정
@@ -12,9 +14,7 @@ const CSSModule = () => {
         <span className="commonColor">안녕하세요.</span>
         <span className={styles.highlight}>yo</span>
       </div>
-      <div className={classNames('menus', ['cls1', 'cls2'], { on: visible })}>
-        메뉴
-      </div>{' '}
+      <div className={cx('menus', ['cls1', 'cls2'], { on: visible })}>메뉴</div>{' '}
       {/**on은 참일때만 추가된다(visible) */}
       <button type="button" onClick={() => setVisible(!visible)}>
         클릭
