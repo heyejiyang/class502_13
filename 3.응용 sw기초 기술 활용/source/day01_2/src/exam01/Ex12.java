8a73a99c4e8098c65733af3f7d235cd217ad02dc
heyejiyang <xoxo99312@naver.com>
2024-04-12 15:44:35 +0900

    형변환 수업예제2

---
+++ 3.응용 sw기초 기술 활용/source/day01_2/src/exam01/Ex12.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex12 {
+    public static void main(String[] args) {
+        int num1 = 13;
+        double num2 = 3.6;
+
+        //곱셈 결과를 정수로 표현
+        //System.out.println((num1*(int)num2));
+        System.out.println((int)(num1*num2)); //double *double -> int
+    }
+}
