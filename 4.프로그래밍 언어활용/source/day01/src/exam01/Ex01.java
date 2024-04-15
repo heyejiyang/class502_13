857357b4578956b191fd694a227cbcef948a9d70
heyejiyang <xoxo99312@naver.com>
2024-04-15 16:20:44 +0900

    객체지향1

--- 4.프로그래밍 언어활용/source/day01/src/exam01/Ex01.java
+++ 4.프로그래밍 언어활용/source/day01/src/exam01/Ex01.java
@@ -1,4 +1,13 @@
 package exam01;
 
 public class Ex01 {
+    public static void main(String[] args) {
+        Student st = new Student();
+
+        st.id = 1999; //인스턴스
+        st.name="김수한무거북이와두루미삼천갑자동방삭치치카포사리사리센타";
+        st.subject="음악";
+
+        st.study();
+    }
 }
