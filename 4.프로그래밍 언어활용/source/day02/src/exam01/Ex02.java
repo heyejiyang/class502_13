9b98523c2337148d866981582c67d1b876ef5819
heyejiyang <xoxo99312@naver.com>
2024-04-16 11:08:45 +0900

    객체지향 프로그래밍1 수업

---
+++ 4.프로그래밍 언어활용/source/day02/src/exam01/Ex02.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        System.out.println(add(5,10));
+    }
+
+    static int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ 4.프로그래밍 언어활용/source/day02/src/exam01/Student.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Student {
+
+    int id;
+    String name;
+    String subject;
+
+    void study(){
+        System.out.printf("%s가 %s를 공부한다.\n",name,subject);
+    }
+}
