038a171fa339179c7c4723b6d56ebb28c9c4ddb3
heyejiyang <xoxo99312@naver.com>
2024-05-10 14:34:57 +0900

    캘린더

--- 4.프로그래밍 언어활용/source/day18/src/exam02/CalEx.java
+++ 4.프로그래밍 언어활용/source/day18/src/exam02/CalEx.java
@@ -25,10 +25,10 @@ public class CalEx {
                 int year = Integer.parseInt(data[0]);//String을 숫자형식으로 바꿈
                 int month = Integer.parseInt(data[1]);
 
-                System.out.printf("----- %d년 %d월 -----%n",year,month); //달력 출력
+                System.out.printf(" ----- %d년 %d월 -----%n",year,month); //달력 출력
 
                 //요일 출력 칸
-                System.out.println("일  월  화  수  목  금  토");
+                System.out.println(" S  M  T  W  T  F  S");
                 //메서드 받아와서 출력시키기 ...
                 getCalendar(year, month);
 
@@ -45,8 +45,8 @@ public class CalEx {
         //년 월을 알고있으면 시작일자를 1로 고정
         LocalDate startDate = LocalDate.of(year, month,1);
 
-        int yoil = startDate.getDayOfWeek().getValue(); //요일의 값 가져오기 1(월) ~ 7(일)
-        yoil = yoil == 7 ? 0 :yoil;
+        int yoil = startDate.getDayOfWeek().getValue() % 7; //요일의 값 가져오기 1(월) ~ 7(일) -> 0 ~ 6
+       // yoil = yoil == 7 ? 0 :yoil;
 
         //5월 1일에서 다음달로 넘어가고(6월1일) -1 하면 마지막 날짜나옴
         LocalDate endDate = startDate.plusMonths(1).minusDays(1);
@@ -57,7 +57,8 @@ public class CalEx {
             System.out.print("   ");
         }
         for(int i = 1, n = yoil + 1; i <= lastDay; i++, n++){
-            System.out.print((i<10)? "  "+ i:" "+i);
+           System.out.print((i<10)? "  "+ i:" "+i);
+
             if(n%7==0) System.out.println(); //토요일 지나면 줄개행
         }
         System.out.println();
