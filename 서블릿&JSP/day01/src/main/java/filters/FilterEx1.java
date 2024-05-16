d4fec92007ae827deb203525e4a43c574e947e46
heyejiyang <xoxo99312@naver.com>
2024-05-16 21:32:24 +0900

    서블릿예습중

---
+++ 서블릿&JSP/day01/src/main/java/filters/FilterEx1.java
@@ -0,0 +1,15 @@
+package filters;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class FilterEx1 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("FilterEx1");
+        filterChain.doFilter(servletRequest,servletResponse);
+        //다음 필터를 체크하게되고 있으면 넘어가고 없으면 마지막으로 응답을 하게된다.
+    }
+}
--- 서블릿&JSP/day01/src/main/java/servlets/HelloServlet.java
+++ 서블릿&JSP/day01/src/main/java/servlets/HelloServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,6 +16,8 @@ public class HelloServlet extends HttpServlet { //웹기술 동작을 위한 상
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //System.out.println("안녕하세요!");
         System.out.println("do get 메서드 !!!");
+        String common1 = req.getServletContext().getInitParameter("common1");
+        System.out.println(common1);
     }
 
     @Override
@@ -22,8 +25,17 @@ public class HelloServlet extends HttpServlet { //웹기술 동작을 위한 상
         System.out.println("destroy!!!");
     }
 
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("init!!!");
+//    }
+
     @Override
-    public void init() throws ServletException {
-        System.out.println("init!!!");
+    public void init(ServletConfig config) throws ServletException {
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s\n",key1,key2);
+
     }
+
 }
