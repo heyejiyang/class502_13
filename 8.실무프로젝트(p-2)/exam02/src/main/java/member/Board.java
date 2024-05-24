4fc074249ea5b4100ad24abed713405140b99663
heyejiyang <xoxo99312@naver.com>
2024-05-24 17:09:25 +0900

    보드

---
+++ 8.실무프로젝트(p-2)/exam02/src/main/java/member/Board.java
@@ -0,0 +1,18 @@
+package member;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class Board {
+    private long seq;
+    private String poster;
+    private String subject;
+    private String content;
+    private LocalDate regDt;
+    private  LocalDate modDt;
+
+}
