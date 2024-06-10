1689d02f3c670b88248b089dbf8ae4fed2fd0536
heyejiyang <xoxo99312@naver.com>
2024-06-10 11:27:39 +0900

    로그인 검증 테스트 _필수항목 검증 공통기능 추가

--- 9.웹 서버 프로그램 구현/day02/src/test/java/member/validators/LoginValidator.java
+++ 9.웹 서버 프로그램 구현/day02/src/test/java/member/validators/LoginValidator.java
@@ -1,5 +1,6 @@
 package member.validators;
 
+import global.exceptions.ValidationException;
 import global.validators.Validator;
 import jakarta.servlet.http.HttpServletRequest;
 import member.services.LoginService;
@@ -9,7 +10,10 @@ public class LoginValidator implements Validator<HttpServletRequest> {
     //통일된 규칙으로 검증 -> 인터페이스 구현체
     @Override
     public void check(HttpServletRequest form) {
-
+        String email = form.getParameter("email");
+        if(email == null || email.isBlank()){
+            throw new ValidationException("이메일을 입력하세요.");
+        }
     }
 
 }
