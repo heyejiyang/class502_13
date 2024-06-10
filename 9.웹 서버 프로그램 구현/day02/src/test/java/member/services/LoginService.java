8196e3c326bbf7b8dc148136095c0f051bd42fca
heyejiyang <xoxo99312@naver.com>
2024-06-10 09:52:18 +0900

    로그인 검증 테스트 가짜객체_호출 테스트

--- 9.웹 서버 프로그램 구현/day02/src/test/java/member/services/LoginService.java
+++ 9.웹 서버 프로그램 구현/day02/src/test/java/member/services/LoginService.java
@@ -1,7 +1,15 @@
 package member.services;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 public class LoginService {
-    public void process(){
+    //HttpServletRequest 인터페이스: 사용자 요청 데이터 조회, 처리
+    public void process(HttpServletRequest request){//사용자가 입력한 데이터 조회할 수 있는 객체 넘겨줌
+        //아이디: email, 비밀번호: password
+        String email = request.getParameter("email");//사용자가 입력한 값 확인할 수 있는 메서드
+        String password = request.getParameter("password");
+
+        System.out.printf("email=%s, password=%s%n", email,password); //값 확인해보기 테스트
 
     }
 }
--- 9.웹 서버 프로그램 구현/day02/src/test/java/tests/LoginServiceTest.java
+++ 9.웹 서버 프로그램 구현/day02/src/test/java/tests/LoginServiceTest.java
@@ -1,30 +1,43 @@
 package tests;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import member.services.LoginService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 @DisplayName("로그인 서비스 기능 테스트")
 public class LoginServiceTest {
 
     private LoginService loginService;
+    private HttpServletRequest request; //사용자 요청 객체
 
     //테스트전에 매번 쓸 객체나 필요할 부분 초기화
     //테스트 할때마다 객체를 생성할것임
     @BeforeEach
     void init(){
         loginService = new LoginService();
+
+        //모의 객체 mock()... 인터페이스 객체 필요
+        //HttpServletRequest 모의 객체 생성
+        request = mock(HttpServletRequest.class);//내가 만들고자 하는 모의객체 정보를 알려줘야함 class 클래스(정보성 객체)
     }
 
     @Test
     @DisplayName("로그인 성공시 예외 발생 없음")
     void successTest(){ //테스트는 항상 통과가 가장 쉬운 테스트부터
         assertDoesNotThrow(()->{
-            loginService.process();
+            loginService.process(request);
         }); //예외 발생 없으면 통과
+    }
+
+    @Test
+    @DisplayName("필수 항목(아이디, 비밀번호) 검증, 검증 실패시 ValidationException 발생")
+    void requiredFieldTest(){
+
     }
 }
