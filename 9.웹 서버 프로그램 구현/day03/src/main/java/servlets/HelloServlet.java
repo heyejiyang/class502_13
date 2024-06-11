564851942beae1e8b7edece176662c9d31111987
heyejiyang <xoxo99312@naver.com>
2024-06-11 16:16:45 +0900

    서블릿

--- 9.웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
+++ 9.웹 서버 프로그램 구현/day03/src/main/java/servlets/HelloServlet.java
@@ -1,4 +1,49 @@
 package servlets;
 
-public class HelloServlet {
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //System.out.println("안녕하세요!");
+        System.out.println("do Get 메서드!!!");
+
+        //모든 서블릿이 조회 가능
+        String common1 = req.getServletContext().getInitParameter("common1");
+        System.out.println(common1);
+        String common2 = req.getServletContext().getInitParameter("common2");
+        System.out.println(common2);
+    }
+
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy!!!");
+    }
+
+//    @Override
+//    public void init() throws ServletException {
+//        System.out.println("init()");
+//    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+        //HelloServlet만 조회 가능
+        String key1 = config.getInitParameter("key1");
+        String key2 = config.getInitParameter("key2");
+        System.out.printf("key1=%s, key2=%s\n",key1,key2);
+
+        //모든 서블릿이 조회 가능
+//        String common1 = config.getInitParameter("common1");
+//        String common2 = config.getInitParameter("common2");
+//        System.out.printf("common1=%s, common2=%s\n",common1,common2);
+    }
 }
---
+++ 9.웹 서버 프로그램 구현/day03/src/main/java/servlets/JoinServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class JoinServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>회원가입</h1>");
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
