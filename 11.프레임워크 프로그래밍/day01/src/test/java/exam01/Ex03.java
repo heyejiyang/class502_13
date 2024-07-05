f1e3e8775eaa9c2317a10d8f62cd41d0451bb8e9
heyejiyang <xoxo99312@naver.com>
2024-07-05 11:59:11 +0900

    두개이상의 설정파일 사용

--- 11.프레임워크 프로그래밍/day01/src/test/java/exam01/Ex03.java
+++ 11.프레임워크 프로그래밍/day01/src/test/java/exam01/Ex03.java
@@ -1,5 +1,6 @@
 package exam01;
 
+import exam01.config.AppCtx;
 import exam01.config.AppCtx2;
 import exam01.member.controllers.RequestJoin;
 import exam01.member.services.InfoService;
@@ -29,6 +30,32 @@ public class Ex03 {
 //        memberDao.getList().forEach(System.out::println);
 
         infoService.printList();
+
+        ctx.close();
+    }
+
+    @Test
+    void test2(){
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx2.class, AppCtx.class);
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("exam01.config");
+
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
+
+        JoinService joinService = ctx.getBean(JoinService.class); //AppCtx2
+        InfoService infoService = ctx.getBean(InfoService.class); //AppCtx2
+
+        RequestJoin form = RequestJoin.builder()
+                .email("user01@test.org")
+                .userName("사용자01")
+                .password("12345678")
+                .confirmPassword("12345678")
+                .build();
+
+        joinService.process(form);
+        infoService.printList();
+
+        Greeter g1 = ctx.getBean(Greeter.class); //AppCtx
+        g1.hello("스프링");
 
         ctx.close();
     }
