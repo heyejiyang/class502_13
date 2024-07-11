34c421477ca3aeed069a2886821a63ec4d79659c
heyejiyang <xoxo99312@naver.com>
2024-07-11 10:48:23 +0900

    mvc세팅 + 커넥션풀설정

--- 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
+++ 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/DBConfig.java
@@ -21,6 +21,13 @@ public class DBConfig {
         /*연결 설정 E*/
 
         /*커넥션 풀 설정 S*/
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        ds.setTestWhileIdle(true); //연결이 유효한지 체크하겠다
+        ds.setMinEvictableIdleTimeMillis(1000 * 60); //유휴객체 살아있는시간 - 기본은 1분(기본값)
+        ds.setTimeBetweenEvictionRunsMillis(1000 * 5);//체크 주기 5초에 한번씩(기본값)
         /*커넥션 풀 설정 E*/
+
+        return ds;
     }
 }
