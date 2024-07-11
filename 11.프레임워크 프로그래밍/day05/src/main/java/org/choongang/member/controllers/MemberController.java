e23ddf68fcbfe82e8b47152153fbf26e8d9ce2c4
heyejiyang <xoxo99312@naver.com>
2024-07-11 10:35:33 +0900

    mvc세팅 + 로거설정

--- 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 
-    @GetMapping("/member/join") //핸들러 매핑이 찾아줌
+    @GetMapping("/member/join") //핸들러 매핑이 요청 url과 매칭되는 컨트롤러 찾아줌
     public String join(){
         return "member/join";
     }
