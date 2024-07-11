22da4d175f0a89815f4161f78f92d5f58269447d
heyejiyang <xoxo99312@naver.com>
2024-07-11 16:18:20 +0900

    MVC

--- 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -3,10 +3,7 @@ package org.choongang.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -17,6 +14,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
+    @Override //정적 경로
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/"); //현재 경로 포함 하위 모든 경로
+    }
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/templates/", ".jsp");
--- 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,14 +1,40 @@
 package org.choongang.member.controllers;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
 
-    @GetMapping("/member/join") //핸들러 매핑이 요청 url과 매칭되는 컨트롤러 찾아줌
-    public String join(){
+    @GetMapping("/join") //핸들러 매핑이 요청 url과 매칭되는 컨트롤러 찾아줌
+//    @RequestMapping(path = "/member/join", method = {RequestMethod.GET,RequestMethod.POST}) //두개의 요청을 가져올 수 있는 상태
+    public String join(Model model, HttpServletRequest request){
+//        model.addAttribute("member", "안녕하세요.");
+//        //jsp에서 el식으로 접근 가능
+//
+//        System.out.println("method: "+request.getMethod());
         return "member/join";
     }
+
+    @PostMapping("/join")//Post방식 처리시 여기로 유입됨
+    public String joinPs(RequestJoin form){
+        return "redirect:/member/login";
+    }
+
+
+//@GetMapping("/member/join")
+//public ModelAndView join(){
+//    ModelAndView mv = new ModelAndView();
+//    mv.addObject("member", "안녕하세요.");
+//    mv.setViewName("member/join");
+//
+//    return mv;
+//}
+
 }
